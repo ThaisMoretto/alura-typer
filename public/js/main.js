58ad94c78b9e7e12f48c6b7decad761ca88bce23
Thais Moretto <thaishelenamoretto@gmail.com>
2020-10-26 18:01:55 -0300

    Andamento ao projeto

--- public/js/main.js
+++ public/js/main.js
@@ -6,8 +6,19 @@ $(function() {
     inicializaContadores();
     inicializaCronometro();
     inicializaMarcadores();
+   
     $("#botao-reiniciar").click(reiniciaJogo);
+    
     atualizaPlacar();
+    
+    $("#usuarios").selectize({
+        create: true,
+        sortField: 'text'
+    });
+    
+    $(".tooltip").tooltipster({
+        trigger: "custom"
+    });
 });
 
 function atualizaTempoInicial(tempo) {
--- public/js/placar.js
+++ public/js/placar.js
@@ -3,7 +3,7 @@ $("#botao-sync").click(sincronizaPlacar);
 
 function inserePlacar() {
     var corpoTabela = $(".placar").find("tbody");
-    var usuario = "Douglas"
+    var usuario = $("#usuarios").val();
     var numPalavras = $("#contador-palavras").text();
 
     var linha = novaLinha(usuario, numPalavras);
@@ -80,6 +80,13 @@ function sincronizaPlacar() {
 
     $.post("http://localhost:3000/placar", dados, function(){
         console.log("salvou no servidor");
+        $(".tooltip").tooltipster("open").tooltipster("content", "Sucesso ao sincronizar");;
+    }).fail(function(){
+        $(".tooltip").tooltipster("open").tooltipster("content", "Falha ao sincronizar"); 
+    }).always(function(){
+        setTimeout(function() {
+        $(".tooltip").tooltipster("close"); 
+    }, 1200);
     });
 }
 
